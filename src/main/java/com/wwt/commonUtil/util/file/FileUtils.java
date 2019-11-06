cb75593e4f5e047d3cc617c270f91fa07954d91e
wangwt <wangwt@llvision.com>
2019-11-06 16:33:08 +0800

    文件工具类

--- src/main/java/com/wwt/commonUtil/util/file/FileUtils.java
+++ src/main/java/com/wwt/commonUtil/util/file/FileUtils.java
@@ -9,8 +9,10 @@ import java.io.IOException;
 /**
  * 文件操作工具类
  */
+
 public class FileUtils {
     private static final Logger log = LoggerFactory.getLogger(FileUtils.class);
+
     /**
      * 创建目录和文件
      *
@@ -69,6 +71,59 @@ public class FileUtils {
     }
 
     /**
+     * 删除文件夹里面的所有文件
+     *
+     * @param path String  文件夹路径  如  c:/fqf
+     */
+    public void delAllFile(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            return;
+        }
+        if (!file.isDirectory()) {
+            return;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                delAllFile(path + "/" + tempList[i]);//先删除文件夹里面的文件
+                delFolder(path + "/" + tempList[i]);//再删除空文件夹
+            }
+        }
+    }
+
+    /**
+     * 删除文件夹
+     *
+     * @param folderPath String  文件夹路径及名称  如c:/fqf
+     * @return boolean
+     */
+    public void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath);  //删除完里面所有内容
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            File myFilePath = new File(filePath);
+            myFilePath.delete();  //删除空文件夹
+
+        } catch (Exception e) {
+            System.out.println("删除文件夹操作出错");
+            e.printStackTrace();
+
+        }
+
+    }
+
+    /**
      * 获取目录的大小
      *
      * @param directory
@@ -95,60 +150,18 @@ public class FileUtils {
     }
 
 
-//    // 链接url下载图片
-//	public static String saveImageForUri(String uri, String filePath, HttpServletRequest request) {
-//        String num = UUID.randomUUID().toString();
-//		String pathName = filePath + "/" + num + ".jpg";
-//        String path = request.getSession().getServletContext().getRealPath("/") + pathName;
-//        FileOutputStream fileOutputStream = null;
-//        DataInputStream dataInputStream = null;
-//        try {
-//            Request.Builder builder = new Request.Builder().url(uri);
-//            Call call = new OkHttpClient().newCall(builder.build());
-//            Response response = call.execute();
-//            InputStream in = response.body().byteStream();
-//            dataInputStream = new DataInputStream(in);
-//
-//            fileOutputStream = new FileOutputStream(new File(path));
-//            ByteArrayOutputStream output = new ByteArrayOutputStream();
-//
-//            byte[] buffer = new byte[1024];
-//            int length;
-//
-//            while ((length = dataInputStream.read(buffer)) > 0) {
-//                output.write(buffer, 0, length);
-//            }
-//            fileOutputStream.write(output.toByteArray());
-//        } catch (MalformedURLException e) {
-//            log.info("图片无法访问");
-//        } catch (IOException e) {
-//            log.info("io异常");
-//        } finally {
-//            try {
-//                dataInputStream.close();
-//            } catch (IOException e) {
-//                log.info("断开连接失败");
-//            }
-//            try {
-//                fileOutputStream.close();
-//            } catch (IOException e) {
-//                log.info("断开连接失败");
-//            }
-//        }
-//        return pathName;
-//    }
-
     /**
      * 递归遍历
+     *
      * @param file
      */
-    public static void getAllFiles(File file){
-        for (File f : file.listFiles()){
-            if(f.isDirectory()){
+    public static void getAllFiles(File file) {
+        for (File f : file.listFiles()) {
+            if (f.isDirectory()) {
                 System.out.println(f.getAbsolutePath());
                 getAllFiles(f);
-            }else{
-                if(f.getName().endsWith(".java")){
+            } else {
+                if (f.getName().endsWith(".java")) {
                     System.out.println(f.getName());
                 }
 
@@ -156,7 +169,46 @@ public class FileUtils {
         }
     }
 
+    /**
+     * 单个文件移动、重命名
+     * 目标文件的上级文件夹必须存在否则会移动失败
+     *
+     * @param sourceFileFullName 源文件 文件全路径名称
+     * @param targetFileFullName 目标文件 文件全路径名称
+     * @return
+     */
+    public static boolean moveFile(String sourceFileFullName, String targetFileFullName) {
+        //源文件
+        boolean flag = false;
+        File sourceFile = new File(sourceFileFullName);
+        if (sourceFile.exists() && sourceFile.isFile()) {
+            File file = new File(targetFileFullName);
+            //源文件移动至目标文件目录
+            if (sourceFile.renameTo(file)) {
+                System.out.println("File is moved successful!");//输出移动成功
+                flag = true;
+            } else {
+                System.out.println("File is failed to move !");//输出移动失败
+            }
+        }
+        return flag;
+    }
+    /**
+     * 文件夹或文件移动、重命名
+     *
+     * @param sourceFilePath
+     * @param targetFilePath
+     * @return
+     */
+    public static boolean moveDirectory(String sourceFilePath, String targetFilePath) {
+//        if(sourceFilePath)
+        return false;
+    }
+
+
+
     public static void main(String[] args) {
-        getAllFiles(new File("D:\\Program Files\\JetBrains\\IntelliJ IDEA 2018.3.4\\workspace_work\\anfang1.3\\"));
+        System.out.println(moveFile("D:/测试照片/moveFile.jpg", "D:/测试照片/moveFile/moveFile.jpg"));
+
     }
 }
